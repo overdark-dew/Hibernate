fe8b605d0187946057e3e8628eb994f2afc71c3b
Overdark <Overdark@192.168.10.96>
2016-10-27 17:23:58 +0300

    Some commit

--- src/main/java/Start/Main.java
+++ src/main/java/Start/Main.java
@@ -3,16 +3,16 @@ package Start;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Iterator;
-import util.HibernateUtil;
+//import util.HibernateUtil;
 //import java.util.Scanner;
 
 
 import Tables.User;
 
-@SuppressWarnings("unused")
+
 public class Main {
 
-	public static void main(String[] args) throws SQLException, NullPointerException {
+	public static void main(String[] args) throws SQLException {
 
 		Collection<User> users = Factory.getInstance().getUserDAO().getAllUser();
 		System.out.println(users);
@@ -23,7 +23,6 @@ public class Main {
 			User user = (User) iterator.next();
 			System.out.println("Пользователь : " + user.getName() + "  ID : " + user.getId());
 			
-
 		}
 	}
 	
